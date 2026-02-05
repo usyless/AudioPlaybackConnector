598ac237443de3c53a4e9bd4972f4b900e68c1ed
usy <67288004+usyless@users.noreply.github.com>
2026-02-05 22:53:58 +0000

    update pch.h

--- pch.h
+++ pch.h
@@ -1,16 +1,9 @@
-// pch.h: This is a precompiled header file.
-// Files listed below are compiled only once, improving build performance for future builds.
-// This also affects IntelliSense performance, including code completion and many code browsing features.
-// However, files listed here are ALL re-compiled if any one of them is updated between builds.
-// Do not add files here that you will be updating frequently as this negates the performance advantage.
-
 #ifndef PCH_H
 #define PCH_H
 
 #include "targetver.h"
 
 // Windows Header Files
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
 #include <windowsx.h>
 #include <commctrl.h>
@@ -29,6 +22,9 @@
 #ifndef _DEBUG
 #define RESULT_DIAGNOSTICS_LEVEL 1
 #endif
+
+// usylibpp (includes wil)
+#include <usylibpp/usylibpp.hpp>
 
 #include <wil/common.h>
 #include <wil/result.h>
